b523b95b13cb9b58f53c7eedfa71d1c49bddda2b
Katja73 <kurnus@luukku.com>
2015-05-10 21:27:04 +0300

    Lisätty ensimmäinen luokka

---
+++ lankaohjelma/src/main/java/lankaohjelma/lankaohjelma/Lankaohjelma.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lankaohjelma.lankaohjelma;
+
+/**
+ *
+ * @author Katja
+ */
+public class Lankaohjelma {
+    
+      /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Katjan lankamaailma");
+        System.out.println("Ei vielä mitään koodia");
+        
+        // Kysytään, haluaako aloittaa tai jatkaa työtä, vai syöttää lankoja
+        
+        // Valitun mukaan jatketaan
+    }
+    
+}
